c18a59ef0f84a5202ecc3cd71fd380b5c61cc0f2
liudonghua123 <liudonghua123@gmail.com>
2024-10-23 22:17:50 +0800

    remove global-shortcut

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -4,7 +4,6 @@ pub fn run() {
         .plugin(tauri_plugin_clipboard_manager::init())
         .plugin(tauri_plugin_http::init())
         .plugin(tauri_plugin_store::Builder::new().build())
-        .plugin(tauri_plugin_global_shortcut::Builder::new().build())
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_process::init())
